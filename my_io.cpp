b479a4c649668dc60f2a2afbdb14444ed3495120
ethansun <ethansun@umich.edu>
2024-11-25 21:28:18 -0500

    cin for double, cout for char

--- my_io.cpp
+++ my_io.cpp
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <fcntl.h>
 #include <string>
 #include <iostream>
 #include <vector>
@@ -20,6 +21,13 @@ public:
         return my_cout;
     }
 
+
+    friend My_Cout& operator<<(My_Cout& my_cout, char ch) {
+        std::string str = std::string(1, ch);
+        my_cout << str;
+        return my_cout;
+    }
+
     friend My_Cout& operator<<(My_Cout& my_cout, int n) {
         std::string str = std::to_string(n);
         my_cout << str;
@@ -27,7 +35,7 @@ public:
     }
 
     friend My_Cout& operator<<(My_Cout& my_cout, double n) {
-        int precision = 100;
+        int precision = 10000;
         int out = n * precision;
         n = (double) out / precision;
         std::string str = std::to_string(n);
@@ -41,70 +49,118 @@ public:
 class My_Cin {
 private:
     int bytesRead;
-    int maxBufSize;
-    std::vector<char> buf;
-    bool more;
+    char curChar;
+    std::vector<char> delimiters;
 public:
     My_Cin() {
         bytesRead = 0;
-        maxBufSize = 100;
-        buf.resize(maxBufSize);
-        more = false;   
+        delimiters = {' ', '\n'};
+        curChar = ' ';
+    }
+
+    ~My_Cin() {
+        // Read everything from the buffer
+        int flags = fcntl(0, F_GETFL, 0);
+        fcntl(0, F_SETFL, flags | O_NONBLOCK);
+
+        read(0, &curChar, 1);
+        while (curChar != '\n') {
+            read(0, &curChar, 1);
+        }
     }
 
     int getBytesRead() {
         return bytesRead;
     }
 
+    bool isDelimiter(char ch) const {
+        for (const auto& elt : delimiters) {
+            if (ch == elt) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     friend My_Cin& operator>>(My_Cin& my_cin, std::string& str) {
         str.clear();
-        int bytesRead = 0;
-        char* data = my_cin.buf.data();
-        
+
         // If left over from previous cin
-        if (my_cin.more) {
-            str += my_cin.buf[0];
-            my_cin.more = false;
+        if (!my_cin.isDelimiter(my_cin.curChar)) {
+            str += my_cin.curChar;
         }
 
-        // Read byte by byte until white space or newline or bufsize reached
-        my_cin.bytesRead += read(0, data, 1);
-        while (*data != ' ' && *data != '\n' && bytesRead != my_cin.maxBufSize) {
-            ++bytesRead;
-            ++data;
-            my_cin.bytesRead += read(0, data, 1);
-        }
-
-        for(int i = 0; i < bytesRead; ++i) {
-            str += my_cin.buf[i];
+        // Read byte by byte until delimiter
+        my_cin.bytesRead += read(0, &my_cin.curChar, 1);
+        while (!my_cin.isDelimiter(my_cin.curChar)) {
+            str += my_cin.curChar;
+            my_cin.bytesRead += read(0, &my_cin.curChar, 1);
         }
 
         return my_cin;
     }
 
     friend My_Cin& operator>>(My_Cin& my_cin, int& n) {
+        // TODO: handle integer overflow
         n = 0;
-        char* data = my_cin.buf.data();
-        int bytesRead = 0;
-        my_cin.bytesRead += read(0, data, 1);
-        while(isdigit(*data) && bytesRead != my_cin.maxBufSize) {
-            n = 10 * n + (*data - '0');
-            my_cin.bytesRead += read(0, data, 1);
-            ++bytesRead;
+
+        // If left over from previous cin
+        if (!my_cin.isDelimiter(my_cin.curChar)) {
+            if (!isdigit(my_cin.curChar)) {
+                return my_cin;
+            }
+            n = 10 * n + my_cin.curChar;
         }
-        if (*data != ' ' || *data != '\n') {
-            my_cin.more = true;
+
+        // Read until a non digit
+        my_cin.bytesRead += read(0, &my_cin.curChar, 1);
+        while(isdigit(my_cin.curChar)) {
+            n = 10 * n + (my_cin.curChar - '0');
+            my_cin.bytesRead += read(0, &my_cin.curChar, 1);
         }
 
         return my_cin;
     }
 
-    // friend My_Cin& operator>>(My_Cin& my_cin, double& n) {
-    //     n = 0;
-    //     int bytesRead = 0;
+    friend My_Cin& operator>>(My_Cin& my_cin, double& n) {
+        n = 0;
 
-    //     while (bytesRead != )
-    // }
+        int numDecimals = 1;
+        bool decimal = false;
+
+        // If left over from previous cin
+        if (!my_cin.isDelimiter(my_cin.curChar) || my_cin.curChar == '.') {
+            if (my_cin.curChar == '.') {
+                decimal = true;
+            }
+            else if (!isdigit(my_cin.curChar)) {
+                return my_cin;
+            }
+            else {
+                n = 10 * n + (my_cin.curChar - '0');
+            }
+        }
+
+        // Read until non digit or not a '.'
+        my_cin.bytesRead += read(0, &my_cin.curChar, 1);
+        while ((isdigit(my_cin.curChar) || my_cin.curChar == '.')) {
+            if (my_cin.curChar == '.') {
+                if (decimal) {
+                    break;
+                }
+                decimal = true;
+            }
+            else {
+                if (decimal) {
+                    numDecimals *= 10;
+                }
+                n = 10 * n + (my_cin.curChar - '0');
+            }
+            my_cin.bytesRead += read(0, &my_cin.curChar, 1);
+        }
+        n /= numDecimals;
+        return my_cin;
+    }
 };
 
 int main() {
@@ -116,11 +172,14 @@ int main() {
     my_cout << 0b0100101 << "\n";
     my_cout << 31.345 << "\n";
     my_cout << 31.000 << "\n";
+    my_cout << 'q' << "\n";
     my_cout << "Bytes printed: " << my_cout.getBytesPrinted() << "\n";
 
     My_Cin my_cin;
     std::string str2;
     int n;
+    double d;
+
     // my_cout << "Enter a string: ";
     // my_cin >> str2;
     // my_cout << "You entered: " << str2 << "\n";
@@ -128,9 +187,14 @@ int main() {
     // my_cin >> n;
     // my_cout << "You entered: " << n << "\n";
 
+    // my_cout << "Enter a double: ";
+    // my_cin >> d;
+    // my_cout << "You entered: " << d << "\n";
+
     my_cout << "Enter something random: ";
-    my_cin >> n >> str;
+    my_cin >> n >> d >> str;
     my_cout << n << "\n";
+    my_cout << d << "\n";
     my_cout << str << "\n";
     
 
--- test.cpp
+++ test.cpp
@@ -3,7 +3,13 @@
 int main() {
     std::string str;
     double n;
-    std::cin >> n >> str;
-    std::cout << n << std::endl;
-    std::cout << str << std::endl;
+    int p;
+    char c;
+    // std::cin >> n >> str >> p;
+    // std::cin >> p >> n >> str;
+    std::cin >> c;
+    std::cout << c << std::endl;
+    // std::cout << p << std::endl;
+    // std::cout << n << std::endl;
+    // std::cout << str << std::endl;
 }
